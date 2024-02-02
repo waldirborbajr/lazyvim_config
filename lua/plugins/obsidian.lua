7ebe78fe110ee43ab398a7726ec0af6aa582e233
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-02 17:16:25 -0300

    Rand Message: /abounding-sand

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -15,6 +15,16 @@ return {
           path = "~/wks/2ndBrain",
         },
       },
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "notes/dailies",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%B %-d, %Y",
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil,
+      },
       completion = {
         -- Set to false to disable completion.
         nvim_cmp = true,
@@ -53,11 +63,31 @@ return {
         -- Currently only telescope supports this.
         new = "<C-x>",
       },
-      -- dir = "~/wks/2ndBrain/Annotation",
-      -- disable_frontmatter = true,
-      -- completion = {
-      --   nvim_cmp = true,
-      -- },
+      attachments = {
+        -- The default folder to place images in via `:ObsidianPasteImg`.
+        -- If this is a relative path it will be interpreted as relative to the vault root.
+        -- You can always override this per image by passing a full path to the command instead of just a filename.
+        img_folder = "assets/imgs", -- This is the default
+        -- A function that determines the text to insert in the note when pasting an image.
+        -- It takes two arguments, the `obsidian.Client` and a plenary `Path` to the image file.
+        -- This is the default implementation.
+        ---@param client obsidian.Client
+        ---@param path Path the absolute path to the image file
+        ---@return string
+        img_text_func = function(client, path)
+          local link_path
+          local vault_relative_path = client:vault_relative_path(path)
+          if vault_relative_path ~= nil then
+            -- Use relative path if the image is saved in the vault dir.
+            link_path = vault_relative_path
+          else
+            -- Otherwise use the absolute path.
+            link_path = tostring(path)
+          end
+          local display_name = vim.fs.basename(link_path)
+          return string.format("![%s](%s)", display_name, link_path)
+        end,
+      },
     },
   },
 }
