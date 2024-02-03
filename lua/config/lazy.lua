c3f66a2746311a42441dc6bf312c6721cec8a526
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-02 23:38:07 -0300

    Rand Message: /slimy-scene

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,10 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.dap.nlua" },
+    -- { import = "lazyvim.plugins.extras.coding.codeium" },
+    -- { import = "lazyvim.plugins.extras.lang.docker" },
 
     -- import/override with your plugins
     { import = "plugins" },
