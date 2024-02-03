b7ad4acf814d0b341e423988e9c725cadff67ce4
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-03 18:00:54 -0300

    Rand Message: /helpless-tent

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -9,21 +9,20 @@ return {
       "nvim-lua/plenary.nvim",
     },
     opts = {
-      workspaces = {
-        {
-          name = "Notes",
-          path = "~/wks/2ndBrain",
-        },
-      },
+      dir = "~/wks/2ndBrain",
+      notes_subdir = "Notes",
+      -- workspaces = {
+      --   {
+      --     name = "Notes",
+      --     path = "~/wks/2ndBrain",
+      --   },
+      -- },
       daily_notes = {
         -- Optional, if you keep daily notes in a separate directory.
-        folder = "notes/dailies",
-        -- Optional, if you want to change the date format for the ID of daily notes.
-        date_format = "%Y-%m-%d",
-        -- Optional, if you want to change the date format of the default alias of daily notes.
-        alias_format = "%B %-d, %Y",
-        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-        template = nil,
+        folder = "Daily",
+        date_format = "%Y-%m-%d-%A",
+        alias_format = "%A, %B %d, %Y",
+        template = "daily.md",
       },
       completion = {
         -- Set to false to disable completion.
