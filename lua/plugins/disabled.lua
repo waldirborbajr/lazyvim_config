9218976555b9676b8a16d07b90c8c696838122b3
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-02-24 08:19:15 -0300

    Rand Message: /volatile-rate

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,5 @@
 return {
+  { "echasnovski/mini.indentscope", enabled = false },
   -- { "echasnovski/mini.pairs", enabled = false },
   -- { "folke/noice.nvim", enabled = false },
   -- { "rcarriga/nvim-notify", enabled = false },
