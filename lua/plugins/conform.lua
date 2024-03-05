a6ce49c70b925ac27d7df6d694b48fef5797b267
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-05 08:37:58 -0300

    Rand Message: /hissing-zipper

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,27 +1,36 @@
 return {
   {
     "stevearc/conform.nvim",
-    formatters_by_ft = {
-      lua = { "stylua" },
-      go = { "gofmt", "gofumpt", "goimports", "golines" },
-      markdown = { "prettier" },
-      json = { "prettierd" },
-      yaml = { "prettierd" },
-      rust = { "rustfmt" },
-      toml = { "taplo" },
-      sh = { "shfmt" },
-      zsh = { "shfmt" },
-      bash = { "shfmt" },
-      -- Missing Markdownlint
-      ["*"] = { "trim_whitespace", "trim_newlines" },
-    },
-
-    formatters = {
-      shfmt = {
-        prepend_args = { "-i", "2", "-ci" },
+    opts = {
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 3000,
+        async = false, -- not recommended to change
+        quiet = false, -- not recommended to change
+        lsp_fallback = true,
       },
-      beautysh = {
-        prepend_args = { "-i", "2" },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "gofmt", "gofumpt", "goimports", "golines" },
+        markdown = { "prettier" },
+        json = { "prettierd" },
+        yaml = { "prettierd" },
+        rust = { "rustfmt" },
+        toml = { "taplo" },
+        sh = { "shfmt" },
+        python = { "black" },
+        zsh = { "shfmt" },
+        bash = { "shfmt" },
+        ["*"] = { "trim_whitespace", "trim_newlines" },
+      },
+
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2", "-ci" },
+        },
+        beautysh = {
+          prepend_args = { "-i", "2" },
+        },
       },
     },
   },
