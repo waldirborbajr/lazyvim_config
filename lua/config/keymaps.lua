f430572b5271e77278ff6739cd1ae99262508c97
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-05 08:25:51 -0300

    Rand Message: /annoying-whip

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,11 +6,17 @@ local Util = require("lazyvim.util")
 
 local map = require("lazyvim.util").safe_keymap_set
 
---[[ General Mappings ]]
-map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+-- Silently move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("v", "<A-j>", "<cmd>m '>+1<cr>gv=gv", { desc = "Move down" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("v", "<A-k>", "<cmd>m '<-2<cr>gv=gv", { desc = "Move up" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
 
-map("i", "jj", "<Esc>", { desc = "" })
+-- Escape in insert mode
+map("i", "jj", "<esc>")
+map("i", "jk", "<esc>")
 
 -- files
 map("n", "!!", ":qa!<enter>", { desc = "" })
