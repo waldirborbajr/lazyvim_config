a809c47837516a05a7b3b04d22f05ea80bb0adff
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-08 00:29:43 -0300

    Rand Message: /rustic-care

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,4 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
-  },
   {
     "nvim-telescope/telescope-symbols.nvim",
     keys = {
