b9d9ebb5319c714cddee2b4e3d02f85d19640c0e
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-12 11:52:44 -0300

    Rand Message: /filthy-place

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   {
     "catppuccin",
+    as = "catppuccin",
     opts = {
       -- https://www.reddit.com/r/neovim/comments/18eobbj/transparency_catppuccin_with_lazyvim_not_working/
       flavour = "macchiato", -- Flavour must be one of: latte, frappe, macchiato, mocha
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,8 @@ return {
   { "nvimdev/dashboard-nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "mini.surround", enabled = false },
+  { "flash.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false }, -- Switched to Oil
