588023580e9edaf6df54ce5f6bec536d5a5989ae
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-12 11:39:14 -0300

    Rand Message: /psychedelic-coat

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin", "tokyonight", "habamax" } },
   checker = { enabled = true, notify = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
