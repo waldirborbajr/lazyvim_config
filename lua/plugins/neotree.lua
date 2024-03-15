96841948a1337a3ffd5d1dd02cc7ddedef0d7bcf
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-15 12:59:47 -0300

    Rand Message: /ethereal-stocking

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,5 +1,12 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>E",
+      "<cmd>Neotree<cr>",
+      desc = "Nee-Tree File Manager",
+    },
+  },
   opts = function(_, opts)
     opts.window.mappings.o = "open"
     opts.window.width = 27
