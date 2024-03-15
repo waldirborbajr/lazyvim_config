222af069ad8e91648ea57e9b247a09fdba51dc69
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-15 09:38:13 -0300

    Rand Message: /big-nose

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,4 @@
 return {
-  {
-    "catppuccin",
-    as = "catppuccin",
-    opts = {
-      -- https://www.reddit.com/r/neovim/comments/18eobbj/transparency_catppuccin_with_lazyvim_not_working/
-      flavour = "macchiato", -- Flavour must be one of: latte, frappe, macchiato, mocha
-    },
-  },
-
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
@@ -15,4 +6,13 @@ return {
       colorscheme = "catppuccin",
     },
   },
+
+  -- "catppuccin",
+  -- {
+  --   as = "catppuccin",
+  --   opts = {
+  --     -- https://www.reddit.com/r/neovim/comments/18eobbj/transparency_catppuccin_with_lazyvim_not_working/
+  --     flavour = "macchiato", -- Flavour must be one of: latte, frappe, macchiato, mocha
+  --   },
+  -- },
 }
