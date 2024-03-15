0369ae4cbdc4b41169f8b0b1414501478dcbea11
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-15 11:51:49 -0300

    feat: enable neotree to another keybind

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>E",
+        "<cmd>Neotree<cr>",
+        desc = "Nee-Tree File Manager",
+      },
+    },
+  },
+}
