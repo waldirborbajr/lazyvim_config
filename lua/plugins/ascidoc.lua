0df933bc60f6b7cb5bdfb096e45e689ef1ca3a00
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-03-24 18:07:58 -0300

    feat: add asciidoc support

---
+++ lua/plugins/ascidoc.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "tigion/nvim-asciidoc-preview",
+    cmd = { "AsciiDocPreview" },
+    ft = { "asciidoc" },
+    build = "cd server && npm install",
+    opts = {
+      -- Add user configuration here
+    },
+  },
+}
