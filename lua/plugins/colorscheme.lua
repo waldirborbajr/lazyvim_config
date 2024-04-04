892c15550760482c9c2ea611cc451cbe5a969c06
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-03 21:04:13 -0300

    chore: adjust colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,8 @@
 return {
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-frappe",
     },
   },
-
-  -- "catppuccin",
-  -- {
-  --   as = "catppuccin",
-  --   opts = {
-  --     -- https://www.reddit.com/r/neovim/comments/18eobbj/transparency_catppuccin_with_lazyvim_not_working/
-  --     flavour = "macchiato", -- Flavour must be one of: latte, frappe, macchiato, mocha
-  --   },
-  -- },
 }
