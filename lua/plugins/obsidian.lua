af60e576a96ecc7872fb98450cf7de27969fe948
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-19 11:41:35 -0300

    chore: config obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,124 +1,71 @@
 return {
   "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-    "nvim-telescope/telescope.nvim",
-    "nvim-treesitter/nvim-treesitter",
+    -- "hrsh7th/nvim-cmp",
+    -- "nvim-telescope/telescope.nvim",
+    -- "nvim-treesitter/nvim-treesitter",
     -- "preservim/vim-markdown",
   },
-  event = {
-    "BufReadPre " .. vim.fn.expand("~") .. "/wks/2ndBrain/**.md",
-    "BufNewFile " .. vim.fn.expand("~") .. "/wks/2ndBrain/**.md",
-  },
-  commands = {
-    "ObsidianOpen",
-    "ObsidianNew",
-    "ObsidianQuickSwitch",
-    "ObsidianFollowLink",
-    "ObsidianBacklinks",
-    "ObsidianToday",
-    "ObsidianYesterday",
-    "ObsidianTemplate",
-    "ObsidianSearch",
-    "ObsidianLink",
-    "ObsidianLinkNew",
-  },
 
-  opts = {
-    dir = "~/wks/2ndBrain", -- specify the vault location. no need to call 'vim.fn.expand' here
-    completion = { nvim_cmp = true },
+  config = function()
+    require("obsidian").setup({
+      workspaces = {
+        {
+          name = "2ndBrain",
+          path = "$HOME/wks/2ndBrain",
+        },
+      },
+      notes_subdir = "inbox",
+      new_notes_location = "notes_subdir",
 
-    daily_notes = {
-      folder = "Periodic 🌄/Days 🌄",
-      -- Optional, if you want to change the date format for the ID of daily notes.
-      -- date_format = "%Y-%m-%d",
-      -- Optional, if you want to change the date format of the default alias of daily notes.
-      -- alias_format = "%B %-d, %Y",
-    },
+      disable_frontmatter = true,
+      templates = {
+        subdir = "templates",
+        date_format = "%Y-%m-%d",
+        time_format = "%H:%M:%S",
+      },
 
-    disable_frontmatter = true,
-
-    -- TODO: configure to my liking
-    -- Optional, alternatively you can customize the frontmatter data.
-    note_frontmatter_func = function(note)
-      -- This is equivalent to the default frontmatter function.
-      -- local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-      -- -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- -- So here we just make sure those fields are kept in the frontmatter.
-      -- if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-      --   for k, v in pairs(note.metadata) do
-      --     out[k] = v
+      -- name new notes starting the ISO datetime and ending with note name
+      -- put them in the inbox subdir
+      -- note_id_func = function(title)
+      --   local suffix = ""
+      --   -- get current ISO datetime with -5 hour offset from UTC for EST
+      --   local current_datetime = os.date("!%Y-%m-%d-%H%M%S", os.time() - 5*3600)
+      --   if title ~= nil then
+      --     suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      --   else
+      --     for _ = 1, 4 do
+      --       suffix = suffix .. string.char(math.random(65, 90))
+      --     end
       --   end
-      -- end
-      -- return out
-    end,
+      --   return current_datetime .. "_" .. suffix
+      -- end,
 
-    -- Optional, for templates (see below).
-    templates = {
-      subdir = "templates",
-      date_format = "%Y-%m-%d-%a",
-      time_format = "%H:%M",
-    },
-
-    follow_url_func = function(url)
-      vim.fn.jobstart({ "open", url })
-    end,
-
-    -- Optional, set to true if you use the Obsidian Advanced URI plugin.
-    -- https://github.com/Vinzent03/obsidian-advanced-uri
-    use_advanced_uri = true,
-
-    -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
-    open_app_foreground = true,
-    ui = {
-      enable = true, -- set to false to disable all additional syntax features
-      update_debounce = 200, -- update delay after a text change (in milliseconds)
-      -- Define how various check-boxes are displayed
-      checkboxes = {
-        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-        ["x"] = { char = "", hl_group = "ObsidianDone" },
-        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-        -- Replace the above with this if you don't have a patched font:
-        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-
-        -- You can also add more custom ones...
+      -- key mappings, below are the defaults
+      mappings = {
+        -- overrides the 'gf' mapping to work on markdown/wiki links within your vault
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- toggle check-boxes
+        -- ["<leader>ch"] = {
+        --   action = function()
+        --     return require("obsidian").util.toggle_checkbox()
+        --   end,
+        --   opts = { buffer = true },
+        -- },
       },
-      external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-      -- Replace the above with this if you don't have a patched font:
-      -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-      reference_text = { hl_group = "ObsidianRefText" },
-      highlight_text = { hl_group = "ObsidianHighlightText" },
-      tags = { hl_group = "ObsidianTag" },
-      hl_groups = {
-        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-        ObsidianTodo = { bold = true, fg = "#f78c6c" },
-        ObsidianDone = { bold = true, fg = "#89ddff" },
-        ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-        ObsidianTilde = { bold = true, fg = "#ff5370" },
-        ObsidianRefText = { underline = true, fg = "#c792ea" },
-        ObsidianExtLinkIcon = { fg = "#c792ea" },
-        ObsidianTag = { italic = true, fg = "#89ddff" },
-        ObsidianHighlightText = { bg = "#75662e" },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
       },
-    },
-  },
-
-  -- mappings = {
-  --   ["gf"] = require("obsidian.mapping").gf_passthrough(),
-  -- },
-
-  config = function(_, opts)
-    require("obsidian").setup(opts)
-    vim.keymap.set("n", "gd", function()
-      if require("obsidian").util.cursor_on_markdown_link() then
-        return "<cmd>ObsidianFollowLink<CR>"
-      else
-        return "gd"
-      end
-    end, { noremap = false, expr = true })
+    })
   end,
 }
