f58bef25ea439def034a45a0487035f65cad3f9d
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-22 10:35:16 -0300

    chore: add zellij plugin

---
+++ lua/plugins/zellij-navigator.lua
@@ -0,0 +1,13 @@
+return {
+  "swaits/zellij-nav.nvim",
+  lazy = true,
+  enabled = false,
+  event = "VeryLazy",
+  keys = {
+    { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+    { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+    { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+    { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+  },
+  opts = {},
+}
