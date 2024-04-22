976753f7bd1da0f1adcb482fccd737454d478504
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-22 10:52:31 -0300

    wip: zellij

--- lua/plugins/zellij-navigator.lua
+++ lua/plugins/zellij-navigator.lua
@@ -1,13 +1,23 @@
 return {
-  "swaits/zellij-nav.nvim",
-  lazy = true,
-  enabled = false,
-  event = "VeryLazy",
-  keys = {
-    { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
-    { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
-    { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-    { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
-  },
-  opts = {},
+  "Lilja/zellij.nvim",
+  -- If you want to configure the plugin
+  config = function()
+    require("zellij").setup({
+      path = "$HOME/.cargo/bin/zellij", -- Zellij binary path
+      vimTmuxNavigatorKeybinds = true, -- Will set keybinds like <C-h> to left
+    })
+  end,
 }
+-- return {
+--   "swaits/zellij-nav.nvim",
+--   lazy = true,
+--   enabled = false,
+--   event = "VeryLazy",
+--   keys = {
+--     { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+--     { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+--     { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+--     { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+--   },
+--   opts = {},
+-- }
