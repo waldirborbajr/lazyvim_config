b734033769c58fd287b05ce025a4eb9be7301e39
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2024-04-26 08:52:06 -0300

    wip

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,9 @@ return {
     "nvim-telescope/telescope.nvim",
     event = "VimEnter",
     branch = "0.1.x",
+    keys = {
+      { "<leader>ff", false },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
